3edbbc71f113a1e76173237974a2ae78a79b4105
Gino Alajar <gino.alajar.ph@gmail.com>
2024-08-25 20:13:12 +0200

    Initial commit

---
+++ src/main/java/com/cardcost/api/CardcostApiApplication.java
@@ -0,0 +1,13 @@
+package com.cardcost.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardcostApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardcost/api/CardcostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardcost.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardcostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
